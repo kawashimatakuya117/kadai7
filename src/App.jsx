658ce2c5ba65a44a7fdc5486e7d4264ed0d1b834
kawashima <t.kawashima17@gmail.com>
2023-10-26 13:58:28 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+// import
+
+// import "□□□"
+// import "ファイルパス or パッケージ名"
+// import './App.css'
+
+// import ◯◯◯ from "□□□"
+// import 名前 from "ファイル"
+// const 名前 = 0
+
+// import { △△△ } from "□□□"
+// const obj = {a: 0, b: 1}
+// console.log(obj.a);
+
+// const {a, b } = {a: 0, b: 1}
+// console.log(a);
+
+// パッケージを使えるようにしている
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import axios from 'axios';
+
+import { TextField } from "./components/TextField"
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // const [list, setList] = useState([])
+  const [text, setText] = useState("")
+
+  return (
+    <div>
+      aaaa
+    </div>
+  )
+}
+
+export default App
